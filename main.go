3364364c64852593635093e8155ba5aac13bce71
jaracil <pepe.aracil.gomez@gmail.com>
2024-05-13 13:35:05 +0200

    Add random seed option

--- main.go
+++ main.go
@@ -15,43 +15,66 @@ func main() {
 		Label: "Select 12 or 24 words",
 		Items: []string{"12 words", "24 words"},
 	}
-	i, _, err := prompt12.Run()
+	iWords, _, err := prompt12.Run()
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
 		return
 	}
-	// Prompt user for passPhrase with hidden input
-	promptPass := promptui.Prompt{
-		Label: "Enter pass phrase",
-		Mask:  '*',
+	// Prompt user for passPhrase or random seed
+	promptPassOrRandom := promptui.Select{
+		Label: "Select creation method",
+		Items: []string{"Use pass phrase", "Create random seed"},
 	}
-	passPhrase1, err := promptPass.Run()
+	iPassOrRandom, _, err := promptPassOrRandom.Run()
 	if err != nil {
 		fmt.Printf("Prompt failed %v\n", err)
 		return
 	}
-	promptPass2 := promptui.Prompt{
-		Label: "Enter pass phrase again to confirm",
-		Mask:  '*',
-	}
-
-	passPhrase2, err := promptPass2.Run()
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
-	}
-	if passPhrase1 != passPhrase2 {
-		fmt.Println("Pass phrases do not match")
-		return
-	}
-
-	// Get sha256 hash of the passPhrase
-	hash256 := sha256.Sum256([]byte(passPhrase1))
+	// Prompt user for passPhrase or generate random seed
 	var entropy []byte
-	if i == 0 {
-		entropy = hash256[:16]
+	if iPassOrRandom == 0 {
+		promptPass := promptui.Prompt{
+			Label: "Enter pass phrase",
+			Mask:  '*',
+		}
+		passPhrase1, err := promptPass.Run()
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+		promptPass2 := promptui.Prompt{
+			Label: "Enter pass phrase again to confirm",
+			Mask:  '*',
+		}
+
+		passPhrase2, err := promptPass2.Run()
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			return
+		}
+		if passPhrase1 != passPhrase2 {
+			fmt.Println("Pass phrases do not match")
+			return
+		}
+
+		// Get sha256 hash of the passPhrase
+		hash256 := sha256.Sum256([]byte(passPhrase1))
+
+		if iWords == 0 {
+			entropy = hash256[:16]
+		} else {
+			entropy = hash256[:32]
+		}
 	} else {
-		entropy = hash256[:32]
+		if iWords == 0 {
+			entropy, err = bip39.NewEntropy(128)
+		} else {
+			entropy, err = bip39.NewEntropy(256)
+		}
+		if err != nil {
+			fmt.Println("Error generating random entropy")
+			return
+		}
 	}
 	mnemonic, _ := bip39.NewMnemonic(entropy)
 	words := strings.Fields(mnemonic)
